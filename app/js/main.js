54eef7db1c1518f509110f48ffd16a1642adc353
SAY10 <SAY10@SAY10>
2021-10-28 23:28:21 +0300

    upd

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,14 @@ $(document).ready(function(){
   $("p.s:empty").hide();
   $("p.t:empty").hide();
 
+
+  $('.yourClass').slick({
+    arrows:true, // показать стрелки
+    dots:false, // не показывать точки
+    autoplay:true,
+    autoplaySpeed: 3500,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  });
+
 });
